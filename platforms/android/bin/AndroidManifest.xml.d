a5455b348f1b61251b734d5a153b1a7f1034bc1a
Jude Mwenda <judemwenda@gmail.com>
2013-10-21 16:45:26 -0400

    Changes to the .apk and the camera issue

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jude/devcivic/civictracker/platforms/android/bin/AndroidManifest.xml : \
-/Users/jude/devcivic/civictracker/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/Main-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/jude/devcivic/civictracker/platforms/android/bin/Main-debug-unaligned.apk : \
-/Users/jude/devcivic/civictracker/platforms/android/bin/Main.ap_ \
-/Users/jude/devcivic/civictracker/platforms/android/bin/classes.dex \
-/Users/jude/devcivic/civictracker/platforms/android/libs/cordova-3.1.0.jar \
